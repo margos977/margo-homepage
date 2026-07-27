29bb88403606a6cadf7de98f2216510852fd2cda
margos977 <margo.sultenfuss@gmail.com>
2026-07-27 09:37:38 -0400

    Add flower favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -21,6 +21,7 @@ export const Route = createRootRoute({
       },
     ],
     links: [
+      { rel: "icon", type: "image/png", href: "/favicon.png" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com" },
